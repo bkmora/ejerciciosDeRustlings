ca67a82c5d9b736a322b23e20104a17c8c364598
bkmora <110305296+bkmora@users.noreply.github.com>
2022-11-06 21:18:44 -0600

    Add files via upload

---
+++ clippy/target/debug/deps/clippy3-65be278cf872ea04.d
@@ -0,0 +1,7 @@
+C:\Users\biank.DESKTOP-62AU20L\OneDrive\Desktop\Bianka\TEC (Ing. Computación)\IV SEM\LP\tareaRust\rustlings\exercises\clippy\target\debug\deps\clippy3-65be278cf872ea04.rmeta: clippy3.rs
+
+C:\Users\biank.DESKTOP-62AU20L\OneDrive\Desktop\Bianka\TEC (Ing. Computación)\IV SEM\LP\tareaRust\rustlings\exercises\clippy\target\debug\deps\clippy3-65be278cf872ea04.d: clippy3.rs
+
+clippy3.rs:
+
+# env-dep:CLIPPY_ARGS=-D__CLIPPY_HACKERY__warnings__CLIPPY_HACKERY__-D__CLIPPY_HACKERY__clippy::float_cmp__CLIPPY_HACKERY__
